41bf9341ee2cc4dda50a6292495071ca8c012d32
Jacob Fisher <jacobfisher18@gmail.com>
2020-02-21 17:03:03 -0800

    uncomment out the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,24 +12,24 @@ ssh -i "default.pem" ubuntu@3.87.14.203 << 'ENDSSH'
 # /bin/echo -e "\e[36mTHIS_IS_CYAN_TEXT\e[0m"
 
 /bin/echo -e "\e[36mGo to the repo and clean up the old build\e[0m"
-# cd workspace/yntht/
-# rm -rf yntht-backend/build/
+cd workspace/yntht/
+rm -rf yntht-backend/build/
 
 /bin/echo -e "\e[36mPull the newest code from origin master\e[0m"
-# git pull
+git pull
 
 /bin/echo -e "\e[36mInstall dependencies, build frontend, and move it to the backend directory\e[0m"
-# cd yntht-frontend
-# npm install
-# npm run build
-# mv build ../yntht-backend/
+cd yntht-frontend
+npm install
+npm run build
+mv build ../yntht-backend/
 
 /bin/echo -e "\e[36mInstall backend dependencies\e[0m"
-# cd ../yntht-backend
-# npm install
+cd ../yntht-backend
+npm install
 
 /bin/echo -e "\e[36mReload the app with pm2\e[0m"
-# pm2 reload yntht
+pm2 reload yntht
 
 # Exit Server
 ENDSSH
