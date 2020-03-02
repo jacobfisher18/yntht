4a5e9596dea1ed45e894c551ceaaf6bcca6d2d4a
Jacob Fisher <jacobfisher18@gmail.com>
2020-03-01 23:09:22 -0800

    comment

--- yntht-backend/utilities/encrypt.js
+++ yntht-backend/utilities/encrypt.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 
 const encryptPassword = (password) => {
-  const secret = 'abc123';
+  const secret = 'abc123'; // TODO: this as an environment variable
   const mykey = crypto.createCipher('aes-128-cbc', secret);
   let mystr = mykey.update(password, 'utf8', 'hex');
   mystr += mykey.final('hex');
