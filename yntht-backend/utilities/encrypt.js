21d0ae97be27e3328d0bfe91bf91549226609376
Jacob Fisher <jacobfisher18@gmail.com>
2020-03-07 10:46:39 -0800

    encrypt as an environment variable

--- yntht-backend/utilities/encrypt.js
+++ yntht-backend/utilities/encrypt.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 
 const encryptPassword = (password) => {
-  const secret = 'abc123'; // TODO: this as an environment variable
+  const secret = process.env.ENCRYPT_SECRET;
   const mykey = crypto.createCipher('aes-128-cbc', secret);
   let mystr = mykey.update(password, 'utf8', 'hex');
   mystr += mykey.final('hex');
